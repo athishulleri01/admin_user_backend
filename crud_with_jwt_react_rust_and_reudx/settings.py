ac07cba1aec4a78e21ee6c1a0b99722a85c3dc0d
athishulleri01 <athishulleri0000@gmail.com>
2023-12-28 21:27:31 +0530

    final commit

--- crud_with_jwt_react_rust_and_reudx/settings.py
+++ crud_with_jwt_react_rust_and_reudx/settings.py
@@ -87,24 +87,24 @@ WSGI_APPLICATION = 'crud_with_jwt_react_rust_and_reudx.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'react',
-#         'USER': 'postgres',
-#         'PASSWORD': 'ADMIN',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'react',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

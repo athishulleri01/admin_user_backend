7e70fd9dadb92461621012624f36d0c74df5a4d5
athishulleri01 <athishulleri0000@gmail.com>
2024-01-01 22:56:36 +0530

    docker config files added

---
+++ django.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Create migrations"
+python3 manage.py makemigrations 
+echo "=========================="
+
+echo "Migrate"
+python3 manage.py migrate 
+echo "=========================="
+
+echo "Collect static"
+python3 manage.py collectstatic 
+echo "=========================="
+
+# Start Gunicorn
+gunicorn -b 0.0.0.0:8000 dryz.wsgi:application --timeout 300
+
+
+# echo "start server"
+# python3 manage.py runserver 0.0.0.0:8000
+# echo "=========================="
